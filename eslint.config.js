5830f929cb71f98adcae85b22e42b485ab5d84df
Manthan Mallikarjun <nahtnam@gmail.com>
2024-11-24 22:06:25 -0800

    add next eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
 import neostandard, { resolveIgnoresFromGitignore } from "neostandard";
+import nextPlugin from "@next/eslint-plugin-next";
 
-export default neostandard({
-  ignores: resolveIgnoresFromGitignore(),
-  ts: true,
-  noStyle: true,
-});
+export default [
+  ...neostandard({
+    ignores: resolveIgnoresFromGitignore(),
+    ts: true,
+    noStyle: true,
+  }),
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+];
