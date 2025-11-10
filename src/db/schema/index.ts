f43e7846c8c66a36a0de380539f8d40541ed4e14
Manthan Mallikarjun <nahtnam@gmail.com>
2025-11-09 23:49:09 -0800

    remove test table

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,9 +1,2 @@
 /** biome-ignore-all lint/performance/noBarrelFile: schema */
 export * from "./__generated__/auth-schema";
-
-import { sqliteTable } from "drizzle-orm/sqlite-core";
-import { shared } from "./shared";
-
-export const test = sqliteTable("test", {
-  ...shared,
-});
