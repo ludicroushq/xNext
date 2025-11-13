4984d3627ac80bf8dc8a1ff8adef8947fca1a4f8
Manthan Mallikarjun <nahtnam@gmail.com>
2025-11-13 01:35:58 -0800

    update relations

--- src/db/relations/index.ts
+++ src/db/relations/index.ts
@@ -3,6 +3,18 @@ import { defineRelations } from "drizzle-orm";
 import * as schema from "@/db/schema";
 
 export const relations = defineRelations(schema, (r) => ({
+  accounts: {
+    user: r.one.users({
+      from: r.accounts.userId,
+      to: r.users.id,
+    }),
+  },
+  sessions: {
+    user: r.one.users({
+      from: r.sessions.userId,
+      to: r.users.id,
+    }),
+  },
   users: {
     accounts: r.many.accounts({
       from: r.users.id,
